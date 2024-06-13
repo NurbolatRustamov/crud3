eda5ebc793206fe4c50ece621588f8e231264ed8
NurbolatRustamov <165665333+NurbolatRustamov@users.noreply.github.com>
2024-06-13 11:02:55 +0500

    crud

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-// let inputData = document.getElementById("search")
-// let BtnData = document.getElementById("inputBtn")
-// let textData = document.getElementById("textData")
+const AddBtn = document.getElementById('AddBtn')
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('closeBtn')
+const FirstName = document.getElementById('first_name')
+const LastName = document.getElementById('last_name')
+const company = document.getElementById('company')
+const phone = document.getElementById('phone')
+const SubmitBtn = document.getElementById('submit-btn')
+const tbody = document.getElementById('tbody')
+const cancelBtn = document.getElementById('cancel-btn')
 
+AddBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+})
 
+closeBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+})
 
+cancelBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+})
 
-// BtnData.addEventListener('click', () => {
+SubmitBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+})
 
-//     textData.innerHTML += `
-//         <h1 class="text-[white] pl-[50px] pt-[20px] text-[25px]">${inputData.value}</h1>
-//     `
-//     inputData.value = "";
-//     counter++;
-// })
+SubmitBtn.addEventListener('click', () => {
 
+    tbody.innerHTML += `
+    <tr
+              class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 max-[550px]:text-[9px]"
+            >
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                ${FirstName.value}
+              </th>
+              <td class="px-6 py-4">${LastName.value}</td>
+              <td class="px-6 py-4">${company.value}</td>
+              <td class="px-6 py-4">${phone.value}</td>
+              <td class="px-6 py-4 flex gap-[20px] max-[550px]:gap-[10px]">
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-[-20px] max-[550px]:ml-[0]"
+                  >Edit</a
+                >
+                <a href="#" class="font-medium text-[red] hover:underline"
+                  >Delete</a
+                >
+              </td>
+            </tr>
+    `
+    FirstName.value = ""
+    LastName.value = ""
+    FirstName.value = ""
+    FirstName.value = ""
 
-let inputData = document.getElementById("search");
-let BtnData = document.getElementById("inputBtn");
-let textData = document.getElementById("textData");
-
-let counter = 1;
-
-BtnData.addEventListener('click', () => {
-    if (inputData.value.trim() !== "") {
-        textData.innerHTML += `
-            <h1 class="text-[white] pl-[50px] pt-[20px] text-[25px]">${counter}. ${inputData.value}</h1>
-        `;
-        inputData.value = "";
-        counter++;
-    }
-});
+})
